1be4d2d2c1a1cf026be66b5cefff7bd7a12281e8
GerhardP99 <gerhard-99@gmx.at>
2020-07-03 16:16:43 +0200

    removed some unnecessary whitespaces

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
     static void calculateRectArea(Integer width1, Integer height1, Integer width2, Integer height2) {
         int rectArea1 = width1 * height1; // calculating the Rectangle Area
         int rectArea2 = width2 * height2; // calculating the Rectangle Area
-
         System.out.printf("\nRectangle 1: \nWidth 1: %d cm\nHeight 1: %d cm\n\nRectangle 2: \nWidth 2: %d cm\nHeight 2: %d cm\n",width1,height1,width2,height2);
         System.out.printf("\nArea of rectangle 1 = %d cm²\nArea of rectangle 2 = %d cm²\n",rectArea1, rectArea2);
     }
@@ -22,25 +21,18 @@ public class Main {
     static void calculateSquareArea(Integer width, Integer height, Integer a) {
         int squareArea = width * height; // calculating the Square Area
         int squarePerimeter = a + a + a + a; // calculating the Square Perimeter
-
         System.out.printf("\nSquare W/H: %d cm/%d cm\n\nSquare Perimeter: %d cm\n",width,height,a);
         System.out.printf("\nArea of Square  = %d cm²\nPerimeter of Square = %d cm\n",squareArea, squarePerimeter);
     }
 
     public static void main(String[] args) {
         int waitTime = 5000; // 5000ms = 5s change it to your liking, if you don't want a pause between the examples just put a 0 in it =).
-
         System.out.println("\nThis is a program to calculate different types of shapes.\n\nCode Review 1/2. \"calculate the rectangular area.\"\n"); // just a print
-
         firstCalculation();
-
         try {
             Thread.sleep(waitTime); // waiting time
         } catch (InterruptedException ignored) {
         }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         System.out.println("\n/////////////////////////////////////////////");
         System.out.println("//     Code Review 3. \"Rectangle Area 1\"  //"); // just a print
         System.out.println("///////////////////////////////////////////");
